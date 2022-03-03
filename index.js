c01256494c40d8bf8a00360bee69b0b3ebe46243
2tmb2 <59399953+2tmb2@users.noreply.github.com>
2022-03-03 09:39:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const img = document.getElementById("img")
 img.src = `images/image${((Math.floor(Math.random()*26))+1)}.JPG`
-if (window.screen.width/img.width > window.screen.height/img.height) {
+if (window.screen.width/img.height > window.screen.height/img.width) {
     img.classList="wide"
 }
 else {
