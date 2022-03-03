717441ba423e23f62d24a6c8e8b3981f2ea5d145
2tmb2 <59399953+2tmb2@users.noreply.github.com>
2022-03-03 09:26:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const img = document.getElementById("img")
-img.src = `images/image_${Math.floor(Math.random()*6)}.JPG`
+img.src = `images/image${(Math.floor(Math.random()*23))+1}.JPG`
 if (window.screen.width/img.width > window.screen.height/img.height) {
     img.classList="wide"
 }
