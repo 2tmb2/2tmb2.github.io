7371c5abe0cc44f9969dabf76855ed7546c71af9
2tmb2 <59399953+2tmb2@users.noreply.github.com>
2022-03-03 09:47:34 -0600

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const img = document.getElementById("img")
-img.src = `images/image${((Math.floor(Math.random()*26))+1)}.JPG`
-if (window.screen.width/img.height > window.screen.height/img.width) {
-    img.classList="wide"
-}
-else {
-    img.classList="tall"
-}
